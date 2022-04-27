35761022a48cde7824b8e7d92ee424c679c55aee
Ali Yılmaz <aliyilmaz.work@gmail.com>
2022-04-28 02:46:08 +0300

    Added example.

--- timecode.js
+++ timecode.js
@@ -10,7 +10,7 @@
  */
 
 String.prototype.toHHMMSS = function () { 
-    var seconds = parseInt(this, 10); // don't forget the second param
+    var seconds = parseInt(this, 10);
     var hours   = Math.floor(seconds / 3600);
     var minutes = Math.floor((seconds - (hours * 3600)) / 60);
     seconds = seconds - (hours * 3600) - (minutes * 60);
